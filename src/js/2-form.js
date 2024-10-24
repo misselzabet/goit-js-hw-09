4eb7ac31817458ef9c93b2b63e25374d67866dc3
misselzabet <lisa.kovalevskaya1999@gmail.com>
2024-10-24 16:55:07 +0200

    change

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,44 @@
-console.log(form);
+
+let formData = {
+    email: "",
+    message: ""
+};
+
+const form = document.querySelector('.feedback-form');
+const emailInput = form.elements.email;
+const messageInput = form.elements.message;
+
+const STORAGE_KEY = "feedback-form-state";
+
+window.addEventListener('DOMContentLoaded', () => {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+    if (savedData) {
+        formData = JSON.parse(savedData);
+        emailInput.value = formData.email || "";
+        messageInput.value = formData.message || "";
+    }
+});
+
+form.addEventListener('input', (event) => {
+    formData[event.target.name] = event.target.value.trim();
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields");
+        return;
+    }
+
+    console.log(formData);
+
+    form.reset();
+    localStorage.removeItem(STORAGE_KEY);
+    formData = {
+        email: "",
+        message: ""
+    };
+});
